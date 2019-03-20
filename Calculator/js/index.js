c6bfcff00557ada4e902cfc2f4bba1f287677eea
parikhmrishi <parikhmrishi@gmail.com>
2019-03-20 14:02:20 +0530

    Update index.js

--- Calculator/js/index.js
+++ Calculator/js/index.js
@@ -7,7 +7,7 @@ function clearInput() {
 }
 
 function calculateResult() {
-	var result = eval(document.getElementById('inputwindow').value.toFixed(2));
+	var result = eval(document.getElementById('inputwindow').value);
 	if(result == "Infinity")
 		result = "Divide by Zero";
 	document.getElementById('inputwindow').value = result;
